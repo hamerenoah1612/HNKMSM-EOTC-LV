127aae6d8c5571d53410f1006b482421321e4093
HAMERE NOAH KIDANE MIHRET, ST MICHAEL & ABUNE TEKLEHIMANOT ETHIOPIAN ORTHODOX TEWAHEDO CATHEDRAL IN LAS VEGAS <hamerenoah.it@gmail.com>
2025-01-22 10:55:06 -0800

    Update settings.py
    change made on DATABASE_URL os.environ

--- HNKMSMEOTCAPP/settings.py
+++ HNKMSMEOTCAPP/settings.py
@@ -120,24 +120,7 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'HNKMSMEOTCAPP.wsgi.application'
 
-# Database
-# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
-
-#DATABASES = { 'default' : dj_database_url.config()}
-# DATABASES = { 
-#     'default': dj_database_url.config(
-#         default=os.environ.get('DATABASE_URL'),
-#         conn_max_age=600,
-#         ssl_require=False
-#         )
-#     }
+DATABASE_URL= os.environ['DB_DATABASE_URL']
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
