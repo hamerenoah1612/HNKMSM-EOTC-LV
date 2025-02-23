003740420abee405637cafcaf1af8db708a698f0
hamerenoah1612 <hamerenoah.it@gmail.com>
2025-02-23 14:14:12 -0800

    updating wsgi.py

--- HNKMSMEOTCAPP/wsgi.py
+++ HNKMSMEOTCAPP/wsgi.py
@@ -7,12 +7,21 @@ For more information on this file, see
 https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
 
-import os
+# import os
 
+# from django.core.wsgi import get_wsgi_application
+
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'HNKMSMEOTCAPP.settings')
+
+# application = get_wsgi_application()
+
+
+import os
 from django.core.wsgi import get_wsgi_application
+from whitenoise import WhiteNoise
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'HNKMSMEOTCAPP.settings')
 
 application = get_wsgi_application()
-
-
+application = WhiteNoise(application) # Add this line
+application.add_files(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'staticfiles')) # For serving static files in production
